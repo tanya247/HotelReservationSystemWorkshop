5bef179d51a7d9308de3160c5d2b23ea1d7127e8
user name <user email>
2021-05-10 21:49:17 +0530

    UC2 Showed

--- src/main/java/HotelReservationSystem.java
+++ src/main/java/HotelReservationSystem.java
@@ -1,4 +1,7 @@
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +27,40 @@ public class HotelReservationSystem {
             System.out.println("Rate on weekdays for regular customers : " + entry.getValue().getHotelRateRegularWeekDay());
         }
     }
+    public String findCheapestHotel(String fromDate, String toDate) {
+        Map<Integer, ArrayList<Hotel>> rateMap = createRateMap(fromDate, toDate);
+        int minimumRate = Integer.MAX_VALUE;
+        for (Map.Entry<Integer, ArrayList<Hotel>> entry : rateMap.entrySet()) {
+            if (entry.getKey() < minimumRate)
+                minimumRate = entry.getKey();
+        }
+
+        System.out.println(rateMap.get(minimumRate).get(0).getHotelName());
+        System.out.println("Total Rate : " + minimumRate*(numberOfDays(fromDate,toDate)+1));
+        return rateMap.get(minimumRate).get(0).getHotelName();
+    }
+    public static Map<Integer, ArrayList<Hotel>>createRateMap(String fromDate, String toDate) {
+        HashMap<Integer, ArrayList<Hotel>> rateMap = new HashMap<>();
+        int numOfDays = numberOfDays(fromDate, toDate);
+        for (Map.Entry<String, Hotel> entry : hotelMap.entrySet()) {
+            int rent = (entry.getValue().getHotelRateRegularWeekDay() * numOfDays);
+            rateMap.computeIfAbsent(rent, k -> new ArrayList<>()).add(entry.getValue());
+        }
+        return rateMap;
+    }
+
+    public static int numberOfDays(String startDate, String endDate) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMMyyyy");
+        LocalDate from = LocalDate.parse(startDate, formatter);
+        LocalDate to = LocalDate.parse(endDate, formatter);
+        int numOfDays = 0;
+        for (LocalDate date = from; date.isBefore(to); date = date.plusDays(1)) {
+            numOfDays++;
+        }
+
+        return numOfDays;
+    }
+
     public static void main(String[] args) {
 
         System.out.println("Welcome to Hotel Reservation Program");
--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -8,11 +8,18 @@ public class HotelReservationTest {
     HotelReservationSystem hotelReservationSystem = new HotelReservationSystem();
 
     @Test
-    public void whenNewHotelAdded_shouldReturnTrue() {
+    public void whenNewHotelAdded_ShouldReturnTrue() {
         Assertions.assertTrue(hotelReservationSystem.addHotel("Lakewood", 110));
         Assertions.assertTrue(hotelReservationSystem.addHotel("Bridgewood", 160));
         Assertions.assertTrue(hotelReservationSystem.addHotel("Ridgewood", 220));
-        hotelReservationSystem.printHotels();
     }
+    @Test
+    public void whenFindCheapestHotelMethodCalled_shouldReturn_nameOfHotelAndMinimumRent() {
+        Assertions.assertTrue(hotelReservationSystem.addHotel("Lakewood", 110));
+        Assertions.assertTrue(hotelReservationSystem.addHotel("Bridgewood", 160));
+        Assertions.assertTrue(hotelReservationSystem.addHotel("Ridgewood", 220));
+        Assertions.assertEquals("Lakewood", hotelReservationSystem.findCheapestHotel("10Sep2020", "11Sep2020"));
+    }
+
 }
 
